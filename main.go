8c66fb4a714414022e5bc21c8f829c18eea183eb
liasica <magicrolan@qq.com>
2020-03-03 14:14:33 +0800

    更新

--- main.go
+++ main.go
@@ -10,16 +10,23 @@
 package main
 
 import (
+    "bufio"
     "flag"
+    "fmt"
     "log"
+    "net"
     "net/http"
     "net/http/httputil"
     "net/url"
+    "os"
+    "os/exec"
+    "runtime"
+    "strings"
 )
 
 var (
-    h    bool
-    u  string
+    h bool
+    u string
     p string
 )
 
@@ -29,23 +36,82 @@ func init() {
     flag.StringVar(&p, "p", "8123", "请输入代理端口，默认为 8123")
 }
 
-func main() {
-    flag.Parse()
+func openbrowser(url string) {
+    var err error
 
-    if h || u == "" || p == "" {
-        flag.Usage()
-        return
+    switch runtime.GOOS {
+    case "linux":
+        err = exec.Command("xdg-open", url).Start()
+    case "windows":
+        err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+    case "darwin":
+        err = exec.Command("open", url).Start()
+    default:
+        err = fmt.Errorf("unsupported platform")
+    }
+    if err != nil {
+        log.Fatal(err)
     }
 
-    log.Println("订阅地址是:", u)
-    log.Println("代理端口是:", p)
+}
+
+func readInput() string {
+    reader := bufio.NewReader(os.Stdin)
+    text, err := reader.ReadString('\n')
+    if err != nil {
+        log.Fatalln(err)
+    }
+    return strings.ReplaceAll(text, "\n", "")
+}
+
+func getIP() string {
+    var ip net.IP
+    ifaces, _ := net.Interfaces()
+    // handle err
+    for _, i := range ifaces {
+        addrs, _ := i.Addrs()
+        // handle err
+        for _, addr := range addrs {
+            switch v := addr.(type) {
+            case *net.IPNet:
+                ip = v.IP
+            case *net.IPAddr:
+                ip = v.IP
+            }
+            // process IP address
+        }
+    }
+    if ip == nil {
+        return ""
+    }
+    return ip.String()
+}
+
+func main() {
+    // flag.Parse()
+    //
+    // if h || u == "" || p == "" {
+    //     flag.Usage()
+    //     return
+    // }
+
+    fmt.Print("请输入需代理的url: ")
+    u = readInput()
+
+    fmt.Print("请输入代理端口(默认是8123): ")
+    p = readInput()
+    if p == "" {
+        p = "8123"
+    }
+    remote, err := url.Parse(u)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    target, _ := url.Parse(remote.Scheme + "://" + remote.Host)
 
     http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-        remote, err := url.Parse(u)
-        if err != nil {
-            log.Fatalln(err)
-        }
-        proxy := httputil.NewSingleHostReverseProxy(remote)
+
+        proxy := httputil.NewSingleHostReverseProxy(target)
 
         req.URL.Host = remote.Host
         req.URL.Scheme = remote.Scheme
@@ -55,6 +121,19 @@ func main() {
         proxy.ServeHTTP(res, req)
     })
 
-    log.Println("代理启动")
-    log.Fatalln(http.ListenAndServe(":" + p, nil))
+    // http.HandleFunc("/body", func(res http.ResponseWriter, req *http.Request) {
+    //
+    // })
+    ip := getIP()
+    if ip == "" {
+        ip = "你的本地IP"
+    }
+
+    q := remote.RawQuery
+    if q != "" {
+        q = "?" + q
+    }
+
+    fmt.Printf("路由器订阅地址填写 http://%s:%s%s%s\n订阅完成后可关闭，下次订阅或更新前请重新运行", ip, p, remote.Path, q)
+    log.Fatalln(http.ListenAndServe(":"+p, nil))
 }
